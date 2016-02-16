ea375f650c1081bf2ff76bc039b8f2e11dcfd28d
nofoe <nofoe@163.com>
2016-02-17 02:06:54 +0800

    Moved another mask view to IB

--- Remix/CTTableViewCell.swift
+++ Remix/CTTableViewCell.swift
@@ -11,19 +11,23 @@ import UIKit
 class CTTableViewCell: UITableViewCell {
 
     @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var imageMask: UIView!
     
     @IBOutlet weak var titleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        let layerView = UIView(frame: CGRectMake(0, 0, coverImageView.frame.size.width, coverImageView.frame.size.height))
-            
-        layerView.backgroundColor = .blackColor()
-        layerView.alpha = 0.3
-        coverImageView.addSubview(layerView)
+        
+        // Using IB instead
+//        let layerView = UIView(frame: CGRectMake(0, 0, coverImageView.frame.size.width, coverImageView.frame.size.height))
+//        layerView.backgroundColor = .blackColor()
+//        layerView.alpha = 0.3
+//        coverImageView.addSubview(layerView)
+        
         coverImageView.clipsToBounds = true
         coverImageView.contentMode = .ScaleAspectFill
         coverImageView.layer.cornerRadius = 8
+        imageMask.layer.cornerRadius = 8
         
     }
 
